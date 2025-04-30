5dcc63fcb3ca887e957025a08b252e109b393311
Menno <mennosijben@gmail.com>
2025-04-30 15:31:33 +0200

    refactor code to use docker containers

---
+++ udagram-api-user/src/sequelize.ts
@@ -0,0 +1,20 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+import { Dialect } from 'sequelize/types/sequelize';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+
+  'dialect': config.dialect as Dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  },
+  'storage': ':memory:',
+});
