5c87cbc8bdc3d9b697ba8bd05ed62d670df0524a
Menno <mennosijben@gmail.com>
2025-05-01 09:55:53 +0200

    remove credentials

--- udagram-api-user/src/aws.ts
+++ udagram-api-user/src/aws.ts
@@ -1,16 +1,11 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { fromIni } from "@aws-sdk/credential-providers";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { config } from './config/config';
 
-// Configure AWS
-const credentials = fromIni({ profile: config.aws_profile });
-
 // Create S3 client
 export const s3Client = new S3Client({
-  region: config.aws_region,
-  credentials: credentials,
+  region: config.aws_region
 });
 
 // Generates an AWS signed URL for retrieving objects
