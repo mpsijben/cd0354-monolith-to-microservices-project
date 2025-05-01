3a4288f50f82a053bafc0ff1665eade7c2c602fc
Menno <mennosijben@gmail.com>
2025-05-01 11:01:54 +0200

    change api host information

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aaa8baf0278a2433d89e87286a53b800-1625098033.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
